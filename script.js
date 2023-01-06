483482de9e6530a268a36c4d1121786d59927607
Bruno R Garcia <bruno.rogues.garcia@gmail.com>
2023-01-06 10:52:21 -0300

    O pai é bom demais fala tu

---
+++ script.js
@@ -0,0 +1,23 @@
+let allFruitsBox = document.querySelector('.allFruits');
+let fruits = [
+    {
+     fruta:'Banana'
+    },
+    {
+        fruta: 'Morango'
+    },
+    {
+        fruta: 'Goiaba'
+    },
+    {
+        fruta: 'Pêssego'
+    }
+];
+
+for(let i=0; i<fruits.length;i++){
+    let createFruit = document.createElement('button');
+    createFruit.innerText = fruits[i].fruta;
+    createFruit.classList.add('fruit');
+
+    allFruitsBox.appendChild(createFruit);
+}
